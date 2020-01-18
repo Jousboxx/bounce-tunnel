dfecbdb1d13733a53fe2e3248de497fe051a22ef
Edward Carlson <kazuya@comcast.net>
2020-01-18 15:28:04 -0500

    updated organization

--- Assets/KeyControl.cs
+++ Assets/KeyControl.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class KeyControl : MonoBehaviour
 {
-    //Control names in Unity for WASD
-    [SerializeField] private string WKeyName = "Mouse Y";
-    [SerializeField] private string AKeyName = "Mouse Y";
-    [SerializeField] private string SKeyName = "Mouse Y";
-    [SerializeField] private string DKeyName = "Mouse Y";
+    //Control names in Unity for Control keys (not hardcoded so keys are remappable)
+    //[SerializeField] makes the field appear in the unity editor
+    [SerializeField] private string ForwardKeyName = "w";
+    [SerializeField] private string LeftKeyName = "a";
+    [SerializeField] private string BackwardKeyName = "s";
+    [SerializeField] private string RightKeyName = "d";
+    [SerializeField] private string UpKeyName = "q";
+    [SerializeField] private string DownKeyName = "e";
+    [SerializeField] private string BrakeKeyName = "space";
 
     Rigidbody RB;
     private float Speed = 20.0f;
@@ -28,15 +32,41 @@ public class KeyControl : MonoBehaviour
         //Update FacingDirection
         FacingDirection = RB.rotation;
 
-        //Multiply the direction by a straight forward vector to convert the quanternion to a vector, then multiply by speed cause gotta go fast
-        Vector3 ForceToAdd = FacingDirection * Vector3.forward * Speed;
-
-
-        //Apply force on ball on up arrow press 
-        //TODO change this to W
-        if(Input.GetKey(KeyCode.UpArrow))
+        //Apply forces on ball on up directional key press
+        if(Input.GetKey(ForwardKeyName))
         {
-            RB.AddForce(ForceToAdd, ForceMode.Force);
+            RB.AddForce(FacingDirection * Vector3.forward * Speed, ForceMode.Force);
+        }
+
+        if(Input.GetKey(BackwardKeyName))
+        {
+            RB.AddForce(FacingDirection * Vector3.back * Speed, ForceMode.Force);
+        }
+
+        if(Input.GetKey(LeftKeyName))
+        {
+            RB.AddForce(FacingDirection * Vector3.left * Speed, ForceMode.Force);
+        }
+
+        if(Input.GetKey(RightKeyName))
+        {
+            RB.AddForce(FacingDirection * Vector3.right * Speed, ForceMode.Force);
+        }
+
+        if(Input.GetKey(UpKeyName))
+        {
+            RB.AddForce(FacingDirection * Vector3.up * Speed, ForceMode.Force);
+        }
+
+        if(Input.GetKey(DownKeyName))
+        {
+            RB.AddForce(FacingDirection * Vector3.down * Speed, ForceMode.Force);
+        }
+        
+        //Apply force in the opposite direction of the ball's motion on brake key press 
+        if(Input.GetKey(BrakeKeyName)){
+            Vector3 MotionDirection = Vector3.Normalize(RB.velocity);
+            RB.AddForce(-MotionDirection * Speed, ForceMode.Force);
         }
     }
 }
