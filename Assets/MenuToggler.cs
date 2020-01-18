1108895b805c5ec550dee8058db1c81d65578e9d
Big Kahuna Burger <curtislee0592@gmail.com>
2020-01-18 15:35:33 -0500

    Testing UI stuff?

---
+++ Assets/MenuToggler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuToggler : MonoBehaviour
+{
+	public static bool GameIsPaused = false;
+	
+	public GameObject pauseMenuUI;
+
+	// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//when escape is pressed, the object is set to an active state that it isn't currently in
+        if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			if(GameIsPaused){
+				Resume();
+			}
+			else {
+				Pause();
+			}
+		}
+    }
+	void Resume(){
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+		GameIsPaused = false;
+	}
+	void Pause(){
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+	}
+}
