644803f96e0c04eaeaba1bc9a0866476558ac022
Edward Carlson <kazuya@comcast.net>
2020-01-18 14:43:25 -0500

    my commit

---
+++ Assets/bounce-related/bouncePad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bouncePad : MonoBehaviour
+{
+	private Vector3 impulse;
+	public int blast;
+    // Start is called before the first frame update
+    void Start()
+    {
+		var boxCollider = gameObject.AddComponent<BoxCollider>();
+		boxCollider.isTrigger = true;
+		var xAngle = transform.eulerAngles.x * (Mathf.PI/180);
+		var yAngle = transform.eulerAngles.y * (Mathf.PI/180);
+		var zAngle = transform.eulerAngles.z * (Mathf.PI/180);
+
+		impulse = new Vector3(Mathf.Sin(zAngle) * -1 * blast, Mathf.Cos(yAngle) * blast, Mathf.Sin(xAngle) * blast);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+	
+	void OnTriggerEnter(Collider other){
+		other.GetComponent<Rigidbody>().AddForce(impulse, ForceMode.Impulse);
+		print("entered");
+	}
+}
