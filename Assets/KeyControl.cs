a62f478dde3d17609228328734c5ad321a0afb88
Jousboxx <jousboxx@gmail.com>
2020-01-18 14:39:15 -0500

    Added mouse motion and key control
    now you can move

---
+++ Assets/KeyControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyControl : MonoBehaviour
+{
+    //Control names in Unity for WASD
+    [SerializeField] private string WKeyName = "Mouse Y";
+    [SerializeField] private string AKeyName = "Mouse Y";
+    [SerializeField] private string SKeyName = "Mouse Y";
+    [SerializeField] private string DKeyName = "Mouse Y";
+
+    Rigidbody RB;
+    private float Speed = 20.0f;
+
+    //Stores the rotation direction that the playerBall is facing
+    private Quaternion FacingDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Update FacingDirection
+        FacingDirection = RB.rotation;
+
+        //Multiply the direction by a straight forward vector to convert the quanternion to a vector, then multiply by speed cause gotta go fast
+        Vector3 ForceToAdd = FacingDirection * Vector3.forward * Speed;
+
+
+        //Apply force on ball on up arrow press 
+        //TODO change this to W
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            RB.AddForce(ForceToAdd, ForceMode.Force);
+        }
+    }
+}
---
+++ Assets/MouseView.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseView : MonoBehaviour
+{
+
+    //Control names in Unity for mouse X and Y
+    [SerializeField] private string MouseXName = "Mouse X";
+    [SerializeField] private string MouseYName = "Mouse Y";
+
+    //Stores a reference to the ball's RigidBody
+    Rigidbody RB;
+    private float Yaw = 0f;
+    private float Pitch = 0f;
+    private float SpeedX = 2.0f;
+    private float SpeedY = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+        LockCursor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Add mouse motions to the Yaw and Pitch variables
+        Yaw += SpeedX * Input.GetAxis(MouseXName);
+        Pitch -= SpeedY * Input.GetAxis(MouseYName);
+
+        //Transform the angle of this object (the ball)
+        transform.eulerAngles = new Vector3(Pitch, Yaw, 0.0f);
+
+    }
+
+     private void LockCursor(){
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
