3c67dd12983846eb8864b77a39c37290544e6bb0
Kohmei358 <ajis.kkadoya@gmail.com>
2020-01-18 22:32:44 -0500

    Boostbar and crosshair

---
+++ Assets/Menu/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Slider boostBar;
+    public Image Fill;
+    public float maxTime = 10.0f;
+    public static float currTime = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currTime = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        boostBar.value = (currTime / maxTime);
+        if (currTime / maxTime < .333)
+        {
+            Fill.color = Color.red;
+        }
+        else
+        {
+            Fill.color = Color.green;
+        }
+        if (currTime < maxTime)
+        {
+            currTime += Time.deltaTime;
+        }
+    }
+}
