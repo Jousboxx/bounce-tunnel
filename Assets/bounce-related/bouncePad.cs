ea42925d6364fd96d9aa968c0b58bf95ba324b04
Jousboxx <jousboxx@gmail.com>
2020-01-18 22:50:24 -0500

    Better bounce mechanics
    Pounce pads now boost slightly in direction player is facing

--- Assets/bounce-related/bouncePad.cs
+++ Assets/bounce-related/bouncePad.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class bouncePad : MonoBehaviour
 {
-	private Vector3 impulse;
+	private Vector3 impulse1;
+	private Vector3 impulse2;
 	public int Blast;
-
+	private Quaternion PlayerRotation;
 	private Quaternion BouncepadRotation;
 	//private Camera camera;
     // Start is called before the first frame update
@@ -14,15 +15,20 @@ public class bouncePad : MonoBehaviour
     {
 		//Set BouncepadRotation to the rotation of this Bouncepad
 		BouncepadRotation = transform.rotation;
-
-		//Set impulse to the unit vector normal to the Bouncepad face multiplied by Blast
-		impulse = (BouncepadRotation * Vector3.forward * Blast);
-		//camera = Camera.main;
 	}
 	
 	void OnTriggerEnter(Collider other){
+		
+		//Set BouncepadRotation to the rotation of this Bouncepad
+		
+		PlayerRotation = other.transform.rotation;
 
-		other.GetComponent<Rigidbody>().AddForce(impulse, ForceMode.Impulse);
+		//Set impulse to the unit vector normal to the Bouncepad face multiplied by Blast	
+		impulse1 = (PlayerRotation * Vector3.forward * (Blast/2));
+		impulse2 = (BouncepadRotation * Vector3.forward * Blast);
+		print("triggered " + (impulse1 + impulse2));	
+		other.GetComponent<Rigidbody>().AddForce(impulse1, ForceMode.Impulse);
+		//other.GetComponent<Rigidbody>().AddForce(impulse2, ForceMode.Impulse);
 		//camera.GetComponent<CABounce>().Bounced();
 	}
 }
